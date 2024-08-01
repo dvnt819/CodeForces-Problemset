2e8fd389a3d6ef14a5e461784be274270e19e10b
Dhvanit <117103433+dvnt819@users.noreply.github.com>
2024-08-01 12:59:20 +0530

    Pangram.py

---
+++ Pangram.py
@@ -0,0 +1,7 @@
+x=int(input())
+y=set(input().lower())
+z={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+if y == z:
+    print("YES")
+else:
+    print("NO")
